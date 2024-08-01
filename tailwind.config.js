8bf52f38c92e89e0b335a14031db20d820e2b978
konzolaw <iykekonzolaw21@gmail.com>
2024-08-01 12:04:39 +0300

    feat: :sparkles: updated projects page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      raleway: ['Raleway','sans-serif'],
+      lato: ['Lato','sans-serif'],
+      'martel': ['Martel Sans','sans-serif'],
+
+    },
+    colors:{
+      'custom-green': '#95cb48',
+      },
   },
   plugins: [],
 };
