e4c494a586235d3f6c7939e09542a9dfab488651
konzolaw <iykekonzolaw21@gmail.com>
2024-08-02 08:08:15 +0300

    feat: :sparkles: updated the "projects views.py"

--- projects/views.py
+++ projects/views.py
@@ -2,4 +2,18 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "projects/index.html")
+    return render(request, "projects/index.html",
+                  context={
+                      "projects":[
+                          {"title": "New", "description": "old"},
+                          {"title": "New", "description": "old"},
+                          {"title": "New", "description": "old"},
+                          {"title": "New", "description": "old"},
+                          {"title": "New", "description": "old"},
+                      ]
+                  }
+                
+                  
+                  
+                  
+                  )
