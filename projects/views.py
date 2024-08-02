0fcb6d5749205fd8973cdc747651c318e9df6e33
konzolaw <iykekonzolaw21@gmail.com>
2024-08-02 09:02:33 +0300

    feat: :sparkles: added the dynamic display in the projects page

--- projects/views.py
+++ projects/views.py
@@ -5,11 +5,11 @@ def index(request):
     return render(request, "projects/index.html",
                   context={
                       "projects":[
-                          {"title": "New", "description": "old"},
-                          {"title": "New", "description": "old"},
-                          {"title": "New", "description": "old"},
-                          {"title": "New", "description": "old"},
-                          {"title": "New", "description": "old"},
+                          {"title": "New", "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque placerat nibh vitae mauris efficitur, vitae egestas lectus molestie. Duis dictum orci massa, sit amet pulvinar nisi porta vitae. Pellentesque aliquam sem metus, tincidunt mattis sapien tempor vitae. Nullam volutpat sit amet metus id scelerisque. Praesent felis tortor, fringilla ac condimentum a, ornare et sapien. Donec a turpis ultrices, fringilla eros vel, lobortis velit."},
+                          {"title": "New", "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque placerat nibh vitae mauris efficitur, vitae egestas lectus molestie. Duis dictum orci massa, sit amet pulvinar nisi porta vitae. Pellentesque aliquam sem metus, tincidunt mattis sapien tempor vitae. Nullam volutpat sit amet metus id scelerisque. Praesent felis tortor, fringilla ac condimentum a, ornare et sapien. Donec a turpis ultrices, fringilla eros vel, lobortis velit."},
+                          {"title": "New", "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque placerat nibh vitae mauris efficitur, vitae egestas lectus molestie. Duis dictum orci massa, sit amet pulvinar nisi porta vitae. Pellentesque aliquam sem metus, tincidunt mattis sapien tempor vitae. Nullam volutpat sit amet metus id scelerisque. Praesent felis tortor, fringilla ac condimentum a, ornare et sapien. Donec a turpis ultrices, fringilla eros vel, lobortis velit."},
+                          {"title": "New", "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque placerat nibh vitae mauris efficitur, vitae egestas lectus molestie. Duis dictum orci massa, sit amet pulvinar nisi porta vitae. Pellentesque aliquam sem metus, tincidunt mattis sapien tempor vitae. Nullam volutpat sit amet metus id scelerisque. Praesent felis tortor, fringilla ac condimentum a, ornare et sapien. Donec a turpis ultrices, fringilla eros vel, lobortis velit."},
+                          {"title": "New", "description": " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque placerat nibh vitae mauris efficitur, vitae egestas lectus molestie. Duis dictum orci massa, sit amet pulvinar nisi porta vitae. Pellentesque aliquam sem metus, tincidunt mattis sapien tempor vitae. Nullam volutpat sit amet metus id scelerisque. Praesent felis tortor, fringilla ac condimentum a, ornare et sapien. Donec a turpis ultrices, fringilla eros vel, lobortis velit."},
                       ]
                   }
                 
