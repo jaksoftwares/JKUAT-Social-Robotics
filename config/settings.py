440f035a93fb8b018604debc3dccada33335c78b
Karume-lab <danielkarume@outlook.com>
2024-08-14 11:51:41 +0300

    chore: :rocket: change DATABASE env vars to use DATABASE_URL

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -105,14 +106,7 @@ if config("USE_SQLITE", default=True, cast=bool):
     }
 else:
     DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": config("DB_NAME"),
-            "USER": config("DB_USER"),
-            "PASSWORD": config("DB_PASSWORD"),
-            "HOST": config("DB_HOST"),
-            "PORT": config("DB_PORT"),
-        }
+        "default": dj_database_url.parse(config("DATABASE_URL")),
     }
 
 
