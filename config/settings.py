9c9bb82841aab21f616bf0d05ea89830761b750a
Karume-lab <danielkarume@outlook.com>
2024-08-14 12:20:38 +0300

    chore: :rocket: deployment fixes for static files

--- config/settings.py
+++ config/settings.py
@@ -152,17 +152,26 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+
+# For development
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / "static",
+    ]
+else:
+    # For production
+    STATIC_ROOT = f"{BASE_DIR}/staticfiles"
+
+# Additional locations of static files
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    f"{BASE_DIR}/static",
 ]
+
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if (
     not DEBUG
 ):  # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = f"{BASE_DIR}/staticfiles"
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
