67cb458995d7464f517818c3e59efac6e054b536
Jeff Chege <jeffkimani143@gmail.com>
2024-08-23 22:03:11 +0300

    feat: :sparkles: project view page & activites page

--- static/js/index.js
+++ static/js/index.js
@@ -1,17 +1,1 @@
 
-const carousel = document.getElementById('carousel');
-const prevBtn = document.getElementById('prevBtn');
-const nextBtn = document.getElementById('nextBtn');
-
-// Calculate the width of a single slide
-const slideWidth = document.querySelector('.w-full').clientWidth;
-
-// Scroll by the width of a single slide
-prevBtn.addEventListener('click', () => {
-    carousel.scrollBy({ left: -slideWidth, behavior: 'smooth' });
-});
-
-nextBtn.addEventListener('click', () => {
-    carousel.scrollBy({ left: slideWidth, behavior: 'smooth' });
-});
-
