65fde04f233de63f7e82d3255efa274f3424d5b3
jaksoftwares <jaksoftwares05@gmail.com>
2025-02-08 00:03:24 +0300

    fix: :bug: added more information in the projects, responsible computing pages ,with more on about

--- responsible_computing/views.py
+++ responsible_computing/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from projects.models import Project
 
-
 def index(request):
     context = {
-        "projects": Project.objects.filter(category="R"),
+        "projects": Project.objects.filter(category="R"),  # Selected projects (Responsible Computing)
+        "mainprojects": Project.objects.filter(category="P"),  # Featured Highlights
     }
-    return render(request, "responsible_computing/index.html", context=context)
+    return render(request, "responsible_computing/index.html", context)
